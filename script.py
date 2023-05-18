ee6e2ba9599b9781d3876338d6e46a98e3b1b3d1
Michael Cheung <56772737+cheung0@users.noreply.github.com>
2023-05-17 17:23:11 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,11 @@
 # Python script that adds a new column for the day of the week and overwrites original csv file
+# Make sure to change csv file name
+
 # Convert the "Date" column to datetime format
 df["Date"] = pd.to_datetime(df["Date"], format="%m/%d/%Y")
 
 # Add a new column for the day of the week
-df["DayOfWeek"] = df["Date"].dt.day_name()
+df["Day Of Week"] = df["Date"].dt.day_name()
 
 # Save the modified DataFrame to a new CSV file
 df.to_csv("/California Air Quality.csv", index=False)
